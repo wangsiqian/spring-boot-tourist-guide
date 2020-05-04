87c47152a5c8e2921696505a4603c7f11ffd0cc6
wangsiqian <365460912@qq.com>
2020-05-04 17:19:51 +0800

    添加 swagger

--- src/main/java/com/wangsiqian/tourist/place/controller/PlaceController.java
+++ src/main/java/com/wangsiqian/tourist/place/controller/PlaceController.java
@@ -6,6 +6,8 @@ import com.wangsiqian.tourist.place.dto.ListNearByPlacesDTO;
 import com.wangsiqian.tourist.place.representation.PlaceRepresentation;
 import com.wangsiqian.tourist.place.service.PlaceApplicationService;
 import com.wangsiqian.tourist.place.service.PlaceRepresentationService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -17,16 +19,19 @@ import java.util.List;
 @RestController
 @RequestMapping("/v1/place")
 @RequiredArgsConstructor
+@Api("景点API")
 public class PlaceController {
     private final PlaceApplicationService placeApplicationService;
     private final PlaceRepresentationService placeRepresentationService;
 
+    @ApiOperation("添加景点（后续改成 admin 接口）")
     @PostMapping
     public CommonResult<String> createPlace(@RequestBody @Validated CreatePlaceDTO createPlaceDTO) {
         return placeApplicationService.createPlace(createPlaceDTO);
     }
 
     @GetMapping
+    @ApiOperation("查找附近的景点")
     public CommonResult<List<PlaceRepresentation>> listNearbyPlaces(
             @Valid ListNearByPlacesDTO listNearByPlacesDTO) {
         return placeRepresentationService.listNearbyPlaces(listNearByPlacesDTO);
