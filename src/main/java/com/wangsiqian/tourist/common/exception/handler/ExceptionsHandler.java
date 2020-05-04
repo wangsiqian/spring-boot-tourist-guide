52cfa9d5f46683222496ab81d21be7c8359dc98b
Wang SiQian <34145085+wangsiqian@users.noreply.github.com>
2020-05-04 17:09:37 +0800

    handle bind exception (#5)

--- src/main/java/com/wangsiqian/tourist/common/exception/handler/ExceptionsHandler.java
+++ src/main/java/com/wangsiqian/tourist/common/exception/handler/ExceptionsHandler.java
@@ -4,6 +4,7 @@ import com.alibaba.fastjson.JSONObject;
 import com.wangsiqian.tourist.common.exception.ApiException;
 import com.wangsiqian.tourist.common.utils.CommonResult;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.BindException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -26,12 +27,17 @@ public class ExceptionsHandler {
     public CommonResult<List<JSONObject>> handleValidationErrorException(
             MethodArgumentNotValidException exception) {
         log.error(exception.toString());
-        return CommonResult.validationErrorResponse(exception);
+        return CommonResult.validationErrorResponse(exception.getBindingResult());
     }
 
     @ExceptionHandler
     public CommonResult<String> handleException(Exception exception) {
-        exception.printStackTrace();
+        log.error(exception.getClass().getName());
         return CommonResult.falseResponse();
+    }
+
+    @ExceptionHandler
+    public CommonResult<List<JSONObject>> handleBindException(BindException exception) {
+        return CommonResult.validationErrorResponse(exception.getBindingResult());
     }
 }
